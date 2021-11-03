6849b7b1f473e395b8a6b33903d74ac88c2b6dcd
mr-joelM <meyerjoel01@gmail.com>
2021-11-03 14:47:24 +0000

    fix of a problem caused to counting the comments votes

--- src/components/VotingButtonsCom.jsx
+++ src/components/VotingButtonsCom.jsx
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const VotingButtonsCom = () => {
+const VotingButtonsCom = ({review_id, index, setRevComments}) => {
     const [voting, setVoting]= useState (0)
-    
+
+    const UpdateVote = async () => {
+        const { data } = await axios.patch(`https://gamers-parlour.herokuapp.com/api/reviews/${review_id}/comments`,{inc_votes: voting})
+        return data
+    }
+
+    useEffect(()=>{
+        if(voting !== 0){UpdateVote()
+            .then((data)=>{
+                setVoting(0)
+            })}
+        
+    }, [voting])
+
     return (
         <div>
-            
+            <button className="decreaseButton" onClick={(event)=>{
+                // update database
+                setVoting(currVotes => currVotes - 1)
+                setRevComments(comment=>{
+                    const newComment=[...comment]
+                    newComment[index].votes-=0.5
+                    //console.log(newComment[index].votes)
+                    return newComment
+                })
+            }}> - </button>
+            <button className="increaseButton" onClick={(event)=>{
+                // update database
+                setVoting(currVotes => currVotes + 1)
+                setRevComments(comment=>{
+                    const newComment=[...comment]
+                    newComment[index].votes+=0.5
+                    return newComment
+                })
+            }}> + </button>            
         </div>
     );
 };
