0431af95059f771d601a331128d5a93e257dddc9
mr-joelM <meyerjoel01@gmail.com>
2021-11-18 18:58:56 +0000

    reformating of VotingButtonRev

--- src/components/VotingButtonsRev.jsx
+++ src/components/VotingButtonsRev.jsx
@@ -1,47 +1,22 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
-
 const VotingButtons = ({ review_id, setSingleReview}) => {
-    const [voting, setVoting]= useState (0)
    
-    const UpdateVote = async () => {
-        const { data } = await axios.patch(`https://gamers-parlour.herokuapp.com/api/reviews/${review_id}`,{inc_votes: voting})
-        return data
+    const UpdateVote = async (vote) => {
+         await axios.patch(`https://gamers-parlour.herokuapp.com/api/reviews/${review_id}`,{inc_votes: vote})
+        .then(()=>{
+            setSingleReview(review=>{
+                const newReview={...review}
+                newReview.votes+= vote
+                return newReview
+            })
+        })
     }
-
-    useEffect(()=>{
-        if(voting !== 0){UpdateVote()
-            .then((data)=>{
-                setVoting(0)
-            })}
-        
-    }, [voting])
-
-
-
 
     return (
         <div>
-            <button className="decreaseButton" onClick={(event)=>{
-                // update database
-                setVoting(currVotes => currVotes - 1)
-                setSingleReview(review=>{
-                    const newReview={...review}
-                    newReview.votes--
-                    return newReview
-                })
-            }}> - </button>
-            <button className="increaseButton" onClick={(event)=>{
-                // update database
-                setVoting(currVotes => currVotes + 1)
-                setSingleReview(review=>{
-                    const newReview={...review}
-                    newReview.votes++
-                    return newReview
-                })
-            }}> + </button>            
+            <button className="decreaseButton" onClick={(event)=>{UpdateVote(-1)}}> - </button>
+            <button className="increaseButton" onClick={(event)=>{UpdateVote(+1)}}> + </button>            
         </div>
     );
 };
