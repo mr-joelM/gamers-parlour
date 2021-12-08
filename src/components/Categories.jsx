487701749e39a7523290c916466093a55b20b76d
mr-joelM <meyerjoel01@gmail.com>
2021-12-08 14:21:46 +0000

    set up of spinner for loading on Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,26 @@
 import {useState, useEffect} from 'react'; 
 import { Link } from 'react-router-dom'; 
+import Spinner from './Spinner';
 import "../css/all.css";
 import filigree from"../css/filigree.png";
 
 const Categories = () => {
     const [cats, setCats] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(()=>{
+        setIsLoading(true)
         fetch('https://gamers-parlour.herokuapp.com/api/categories' )
         .then((response)=>{return response.json()})
-        .then((data)=>{setCats(data.categories)})
+        .then((data)=>{
+            setCats(data.categories)
+            setIsLoading(false)
+        })
     },[])
 
     window.scrollTo(0, 0)
+
+    if(isLoading)return <Spinner />
 
     return (
         <div className="categories">
