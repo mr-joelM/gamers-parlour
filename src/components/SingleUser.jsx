08e8190fffdf9ddd2e2f11c0a9e07719ad39978b
mr-joelM <meyerjoel01@gmail.com>
2022-03-01 22:31:00 +0000

    box shadow

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -1,6 +1,5 @@
 import {useState, useEffect} from 'react';
-import { useParams } from "react-router-dom"; 
-import { Link } from 'react-router-dom'; 
+import { useParams } from "react-router-dom";
 import "../css/all.css";
 
 const SingleUser = () => {
